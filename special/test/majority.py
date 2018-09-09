1aa282682b2cb3c158d2513dad9a59ab1f50de37
Christian Soler <solero93@gmail.com>
2018-09-09 19:35:06 +0200

    Added Majority problem with solutions

---
+++ special/test/majority.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from src.majority import majority
+
+cases = [
+    (
+        [1], 1
+    ),
+    (
+        [1, 1], 1
+    ),
+    (
+        [1, 1, 2], 1
+    ),
+    (
+        [1, 2, 1], 1
+    ),
+    (
+        [2, 1, 1], 1
+    ),
+    (
+        [2, 2, 1, 1, 1, 2, 2], 2
+    )
+]
+
+
+class MajorityTest(TestCase):
+    def test_cases(self):
+        for numbers, expected in cases:
+            self.assertCountEqual(expected, majority(numbers),
+                                  msg=f'Output for {numbers} -> should be {expected}')
