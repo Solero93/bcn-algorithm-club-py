52390fd028cf3818d2a39ee2e9b7635d1fb3aa9f
Christian Soler <solero93@gmail.com>
2018-09-09 20:44:06 +0200

    Added stones problem and tests

---
+++ special/test/stones.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from src.stones import num_jewels_in_stones
+
+cases = [
+    (
+        "aA", "aAAbbbb", 3
+    ),
+    (
+        "z", "ZZ", 0
+    )
+]
+
+
+class NumbersTest(TestCase):
+    def test_cases(self):
+        for jewels, stones, expected in cases:
+            self.assertCountEqual(expected, num_jewels_in_stones(jewels, stones),
+                                  msg=f'Output for {jewels}, {stones} -> should be {expected}')
