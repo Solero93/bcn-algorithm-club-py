50a80b9d6d4b6988c49efae6040ed8717ee5f009
Pau Baiget <pbaiget@netsuite.com>
2018-10-01 15:41:57 +0200

    Added DP solution from Geeks4geeks for Edit Distance

--- dynamicprogramming/solutions/edit_distance.py
+++ dynamicprogramming/solutions/edit_distance.py
@@ -28,6 +28,37 @@ Replace 'n' with 'r', insert t, insert a
 
 """
 def edit_distance(str1: str, str2: str) -> int:
-    # Hah, busted! The solution will be pushed after the session.
-    # NOW GO BACK TO CODING! :D
-    return 0
+
+    m = len(str1)
+    n = len(str2)
+
+    # Create a table to store results of subproblems
+    dp = [[0 for x in range(n + 1)] for x in range(m + 1)]
+
+    # Fill d[][] in bottom up manner
+    for i in range(m + 1):
+        for j in range(n + 1):
+
+            # If first string is empty, only option is to
+            # insert all characters of second string
+            if i == 0:
+                dp[i][j] = j  # Min. operations = j
+
+            # If second string is empty, only option is to
+            # remove all characters of second string
+            elif j == 0:
+                dp[i][j] = i  # Min. operations = i
+
+            # If last characters are same, ignore last char
+            # and recur for remaining string
+            elif str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+
+            # If last character are different, consider all
+            # possibilities and find minimum
+            else:
+                dp[i][j] = 1 + min(dp[i][j - 1],  # Insert
+                                   dp[i - 1][j],  # Remove
+                                   dp[i - 1][j - 1])  # Replace
+
+    return dp[m][n]
